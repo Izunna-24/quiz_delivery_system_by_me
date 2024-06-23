8ac826dd446a6d402bfd5cd421064c8ec50960e5
Izuchukwu Ijeudo <izuchukwuijeudo@gmail.com>
2024-06-23 17:12:04 +0100

    quizing you

---
+++ quiz_app/models.py
@@ -0,0 +1,66 @@
+from django.db import models
+
+
+class User(models.Model):
+    USER_CATEGORIES = [
+        ('ST', 'STUDENT'),
+        ('TC', 'TEACHER'),
+        ('FnF', 'FAMILY_AND_FRIENDS'),
+        ('PR', 'PROFESSIONAL'),
+    ]
+    user_category = models.CharField(max_length=1, choices=USER_CATEGORIES, default='ST')
+    username = models.CharField(max_length=100, unique=True)
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.username
+
+
+class Quiz(models.Model):
+    USER_CATEGORIES = [
+        ('ST', 'STUDENT'),
+        ('TC', 'TEACHER'),
+        ('FnF', 'FAMILY_AND_FRIENDS'),
+        ('PR', 'PROFESSIONAL'),
+    ]
+    quiz_owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()
+    time_created = models.DateTimeField(auto_now_add=True)
+    questions = models.TextField()
+
+    def __str__(self):
+        return self.title
+
+
+class Question(models.Model):
+    QUESTION_TYPES = [
+        ('MC', 'Multiple Choice'),
+        ('T/F', 'True or False'),
+        ('ST', 'Short Text'),
+    ]
+    question_type = models.CharField(max_length=1, choices=QUESTION_TYPES, default='MC')
+    question_text = models.TextField()
+    time_created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.question_text
+
+
+class Answer(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    answer = models.CharField(max_length=100)
+    time_created = models.DateTimeField(auto_now_add=True)
+    is_correct = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.answer
+
+
+class Submission(models.Model):
+    quiz = models.ForeignKey(Quiz, on_delete=models.CASCADE)
+    answer_owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    time_submitted = models.DateTimeField(auto_now_add=True)
+    answer = models.ForeignKey(Answer, on_delete=models.CASCADE)
